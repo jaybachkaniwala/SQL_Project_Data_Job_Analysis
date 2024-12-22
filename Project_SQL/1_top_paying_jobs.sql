a4cfb066671cd1edd6ced546271a0803b704876d
jaybachkaniwala <jaybachkaniwala@gmail.com>
2024-12-22 17:26:48 -0500

    File update

--- Project_SQL/1_top_paying_jobs.sql
+++ Project_SQL/1_top_paying_jobs.sql
@@ -34,18 +34,88 @@ Job Title Variety: There's a high degree of diversity in job titles, from Market
 Location: All of the top 10 highest paying business analyst jobs are remote jobs. Based on this we can determine that Dallas based business analyst jobs are paying less than $134,550.
 
 RESULTS FROM THE ABOVE QUERY 
-----------------------
+-----------------------------
 
-"job_id","job_title","job_location","job_schedule_type","salary_year_avg","job_posted_date","company_name"
-502610,"Lead Business Intelligence Engineer","Anywhere","Full-time","220000.0","2023-08-29 18:26:36","Noom"
-112859,"Manager II, Applied Science - Marketplace Dynamics","Anywhere","Full-time","214500.0","2023-12-18 08:02:37","Uber"
-998056,"Analyst","Anywhere","Full-time","200000.0","2023-10-04 11:01:48","Multicoin Capital"
-1069582,"Analyst","Anywhere","Full-time","200000.0","2023-12-21 13:01:17","Multicoin Capital"
-17458,"Senior Economy Designer","Anywhere","Full-time","190000.0","2023-06-21 18:11:35","Harnham"
-416185,"Staff Revenue Operations Analyst","Anywhere","Full-time","170500.0","2023-12-21 09:50:43","Gladly"
-1099753,"REMOTE - Business Intelligence Analyst (Leadership Role) - GCP","Anywhere","Full-time","162500.0","2023-10-11 12:01:45","CyberCoders"
-1313937,"Manager Analytics and Reporting","Anywhere","Full-time","145000.0","2023-06-22 13:01:41","CyberCoders"
-106225,"Business Strategy Analyst Senior (Hybrid)","Anywhere","Full-time","138640.0","2023-06-30 14:01:54","USAA"
-661103,"Marketing Analytics Manager","Anywhere","Full-time","134550.0","2023-08-03 19:59:22","Get It Recruit - Marketing"
+"job_id": 502610
+"job_title": "Lead Business Intelligence Engineer" 
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "220000.0"
+"job_posted_date": "2023-08-29 18:26:36"
+"company_name": "Noom"
+
+"job_id": 112859
+"job_title": "Manager II, Applied Science - Marketplace Dynamics" 
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "214500.0"
+"job_posted_date": "2023-12-18 08:02:37"
+"company_name": "Uber"
+
+"job_id": 998056
+"job_title": "Analyst" 
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "200000.0"
+"job_posted_date": "2023-10-04 11:01:48"
+"company_name": "Multicoin Capital"
+
+"job_id": 1069582
+"job_title": "Analyst" 
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "200000.0"
+"job_posted_date": "2023-12-21 13:01:17"
+"company_name": "Multicoin Capital"
+
+"job_id": 17458
+"job_title": "Senior Economy Designer"
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "190000.0"
+"job_posted_date": "2023-06-21 18:11:35"
+"company_name": "Harnham"
+
+"job_id": 416185
+"job_title": "Staff Revenue Operations Analyst"
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "170500.0"
+"job_posted_date": "2023-06-21 18:11:35"
+"company_name": "Gladly"
+
+"job_id": 1099753
+"job_title": "REMOTE - Business Intelligence Analyst (Leadership Role) - GCP"
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "162500.0"
+"job_posted_date": "2023-10-11 12:01:45"
+"company_name": "CyberCoders"
+
+"job_id": 1313937
+"job_title": "Manager Analytics and Reporting"
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "145000.0"
+"job_posted_date": "2023-06-22 13:01:41"
+"company_name": "CyberCoders"
+
+"job_id": 106225
+"job_title": "Business Strategy Analyst Senior (Hybrid)"
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "138640.0"
+"job_posted_date": "2023-06-30 14:01:54"
+"company_name": "USAA"
+
+"job_id": 661103
+"job_title": "Marketing Analytics Manager"
+"job_location": "Anywhere"
+"job_schedule_type": "Full-time"
+"salary_year_avg": "13455.0"
+"job_posted_date": "2023-08-03 19:59:22" 
+"company_name": "Get It Recruit - Marketing"
 */
+
+
 
